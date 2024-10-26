36c21946599f3e46e01a306de5003ab45c2d8cc9
Nonthakorn <nnonthakornn@gmail.com>
2024-10-26 20:16:12 +0700

    add ft_lstnew and print_list

--- pushswap_utils.c
+++ pushswap_utils.c
@@ -11,3 +11,36 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+t_list *ft_lstnew(int value)
+{
+	t_list *new_node;
+
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->value = value;
+	new_node->next = NULL;
+
+	return (new_node);
+}
+
+void print_list(t_list *head)
+{
+	t_list *ptr;
+
+	ptr = head;
+	while(ptr)
+	{
+		ft_printf("%d", ptr->value);
+		ptr = ptr->next;
+	}
+}
+
+int main()
+{
+	t_list *node1;
+
+	node1 = ft_lstnew(2);
+	print_list(node1);
+}
